7c01139483e8ca06952ee3314b63150331ca7e84
Andy Mention <amention@gmail.com>
2018-11-15 22:17:15 -0500

    Add authentication with devise
    via this blog post:
    https://codepany.com/blog/rails-5-user-accounts-with-3-types-of-roles-devise-rails_admin-cancancan/

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :lists
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  root to: "home#index"
 end
