7eadd68b27ffea3e06ea55d154cf90ad5197ef06
Andy Mention <amention@gmail.com>
2018-11-18 18:52:47 -0500

    Groups and Users have each other through Lists
    I was fighting this for a while, but it's dumb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable
   has_many :lists
+  has_many :groups, through: :lists
 
   validates :name, presence: true
 end
