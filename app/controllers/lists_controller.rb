ba35fff7c602a9cec14423e727f20d396f80186d
Andy Mention <amention@gmail.com>
2018-11-18 19:29:24 -0500

    Create personal and public list show pages

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,5 +5,14 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    if @list.user == current_user
+      render "personal_show"
+    else
+      render "public_show"
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
   end
 end
