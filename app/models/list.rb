5bad3f4ee376b21d637058fa143b6e9eb6df8b11
Andy Mention <amention@gmail.com>
2018-12-05 12:35:36 -0700

    Update Public lists
    This commit removes item names from lists, orders lists by availability,
    and makes URLs clickable

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,8 @@ class List < ApplicationRecord
   has_and_belongs_to_many :items
   belongs_to :user
   belongs_to :group
+
+  def items_ordered_by_availability
+    items.sort_by{|i| i.available? ? 0 : 1}
+  end
 end
