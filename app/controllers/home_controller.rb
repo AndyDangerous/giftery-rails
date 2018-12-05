ffd4d9b84e0db2acf9865615e30fa25f66b67f15
Andy Mention <amention@gmail.com>
2018-12-05 08:59:53 -0700

    Set after_sign_in_path
    This wasn't doing anything because it had a typo and was in the wrong
    place

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
-  def after_sign_in_apth_for(resource)
-    items_path
-  end
-
   def index
   end
 end
