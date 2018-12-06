e97a0334d320571b037bf0349f4f0d142304126e
Andy Mention <amention@gmail.com>
2018-12-05 21:58:26 -0700

    Users can create new groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,19 @@
 class GroupsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    @group = Group.create(group_params)
+
+    if @group.save
+      current_user.groups << @group
+      redirect_to @group
+    else
+      render 'new'
+    end
+  end
+
   def index
     @groups = Group.all
   end
@@ -7,5 +21,11 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
     @user_lists = @group.user_lists(except: current_user)
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
